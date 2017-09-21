33c48f1824ebb7426d95e7e1b3bec2e3e791333c
Roger Arques <rarquesvall@gmail.com>
2017-09-21 16:22:48 +0200

    Steps for scenarios, implementation pending

---
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateCourtSteps.java
@@ -0,0 +1,25 @@
+package cat.udl.eps.softarch.mypadel.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+
+public class CreateCourtSteps {
+    @And("^A new court is available$")
+    public void aNewCourtIsAvailable() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I create a new court$")
+    public void iCreateANewCourt() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^A new court has not been created$")
+    public void aNewCourtHasNotBeenCreated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/RemoveCourtSteps.java
@@ -0,0 +1,37 @@
+package cat.udl.eps.softarch.mypadel.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+
+public class RemoveCourtSteps {
+    @And("^There is an existing court$")
+    public void thereIsAnExistingCourt() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I remove a court$")
+    public void iRemoveACourt() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^A court is unavaliable$")
+    public void aCourtIsUnavaliable() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The court does not exist$")
+    public void theCourtDoesNotExist() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The court is avaliable$")
+    public void theCourtIsAvaliable() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
