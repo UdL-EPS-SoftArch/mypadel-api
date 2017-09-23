4fab37e1583b5028306c0cc4cdebfd0f072ef910
Marc Ribalta Gene <mariva1995@gmail.com>
2017-09-23 15:42:32 +0200

    id created autoincrement

--- src/main/java/cat/udl/eps/softarch/mypadel/domain/Match.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/Match.java
@@ -1,18 +1,29 @@
 package cat.udl.eps.softarch.mypadel.domain;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.time.Duration;
 import java.util.Date;
 
 public class Match extends UriEntity<String> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     private Date startDate;
+
     private Duration duration;
+
     private Enum courtType;
+
     private Date cancelationDeadline;
 
     @Override
     public String getId() {
-        return null;
+        return Long.toString(id);
     }
 
     public Date getStartDate() {
