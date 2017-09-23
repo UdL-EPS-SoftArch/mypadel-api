57b3cbc93952b645d7f4d3b8390d8acb5e8b8f68
Marc Ribalta Gene <mariva1995@gmail.com>
2017-09-23 13:43:33 +0200

    Corrected flag

--- src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateMatchStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateMatchStepDefs.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.mypadel.steps;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CreateMatchStepDefs {
@@ -9,7 +10,7 @@ public class CreateMatchStepDefs {
     @Autowired
     private StepDefs stepDefs;
 
-    @And("^I click into create public match option$")
+    @When("^I click into create public match option$")
     public void aNewCourtIsAvailable() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
