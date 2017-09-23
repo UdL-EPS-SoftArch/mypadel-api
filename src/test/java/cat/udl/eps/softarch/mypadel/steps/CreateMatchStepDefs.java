b8eac3b38637bd7aa8ad613f498461b7018cc3ce
Marc Ribalta Gene <mariva1995@gmail.com>
2017-09-23 16:21:27 +0200

    modified the wroten steps and completed one of them

--- src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateMatchStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateMatchStepDefs.java
@@ -1,25 +1,39 @@
 package cat.udl.eps.softarch.mypadel.steps;
 
+import cat.udl.eps.softarch.mypadel.domain.Match;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.time.Duration;
+import java.util.Date;
+
+import static cat.udl.eps.softarch.mypadel.steps.AuthenticationStepDefs.authenticate;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 public class CreateMatchStepDefs {
 
     @Autowired
     private StepDefs stepDefs;
 
-    @When("^I click into create public match option$")
-    public void iClickIntoCreatePublicMatchOption() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @And("^I confirm with the correct match parameters$")
-    public void iConfirmWithTheCorrectMatchParameters() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I create a new match$")
+    public void iCreateANewMatch() throws Throwable {
+        Match match = new Match();
+        match.setStartDate(new Date(2017, 10, 1));
+        match.setCourtType(true);
+        match.setDuration(Duration.ofMinutes(30));
+        match.setCancelationDeadline(new Date(2017, 9, 30));
+        String message = stepDefs.mapper.writeValueAsString(match);
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/match")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(message)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(authenticate()))
+                .andDo(print());
     }
 
     @And("^A match has been created$")
--- src/test/resources/features/CreateMatch.feature
+++ src/test/resources/features/CreateMatch.feature
@@ -5,12 +5,11 @@ Feature: Create match
 
   Scenario: A user creates a public match
     Given I login as "user" with password "password"
-    When I click into create public match option
-    And I confirm with the correct match parameters
+    When I create a new match
     Then The response code is 201
     And A match has been created
 
   Scenario: Unlogged person tries to create a match
     Given I'm not logged in
-    When I click into create public match option
+    When I create a new match
     Then The response code is 401
