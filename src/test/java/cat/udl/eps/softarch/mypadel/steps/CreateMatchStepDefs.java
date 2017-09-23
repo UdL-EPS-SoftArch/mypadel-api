f272396c47f8c94a9bdb2ea05bfb0f168cd898da
Marc Ribalta Gene <mariva1995@gmail.com>
2017-09-23 13:42:32 +0200

    Inserted step definitions

---
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateMatchStepDefs.java
@@ -0,0 +1,23 @@
+package cat.udl.eps.softarch.mypadel.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CreateMatchStepDefs {
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @And("^I click into create public match option$")
+    public void aNewCourtIsAvailable() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^A match has been created$")
+    public void aMatchHasBeenCreated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
