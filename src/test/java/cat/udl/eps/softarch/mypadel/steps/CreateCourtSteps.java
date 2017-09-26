3e9640a5128b4758ad83cc5839b4938d7119864d
Roger Arques <rarquesvall@gmail.com>
2017-09-26 19:14:16 +0200

    Scenarios & steps for creating indoor and outdoor courts

--- src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateCourtSteps.java
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/CreateCourtSteps.java
@@ -20,19 +20,10 @@ public class CreateCourtSteps {
     private StepDefs stepDefs;
 
 
-    @And("^A new court is available$")
-    public void aNewCourtIsAvailable() throws Throwable {
-        stepDefs.result = stepDefs.mockMvc.perform(
-                get("/courts/1")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(authenticate()))
-                .andDo(print())
-                .andExpect(jsonPath("$.available", is(true)));
-    }
-
-    @When("^I create a new court$")
+    @When("^I create a new indoor court$")
     public void iCreateANewCourt() throws Throwable {
         Court court = new Court();
+        court.setIndoor(true);
         String message = stepDefs.mapper.writeValueAsString(court);
         stepDefs.result = stepDefs.mockMvc.perform(
                 post("/courts")
@@ -43,6 +34,42 @@ public class CreateCourtSteps {
                 .andDo(print());
     }
 
+    @And("^A new indoor court is available$")
+    public void aNewCourtIsAvailable() throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/courts/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.available", is(true)))
+                .andExpect(jsonPath("$.indoor", is(true)));
+    }
+
+
+    @When("^I create a new outdoor court$")
+    public void iCreateANewOutdoorCourt() throws Throwable {
+        Court court = new Court();
+        court.setIndoor(false);
+        String message = stepDefs.mapper.writeValueAsString(court);
+        stepDefs.result = stepDefs.mockMvc.perform(
+                post("/courts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(message)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(authenticate()))
+                .andDo(print());
+    }
+
+    @And("^A new outdoor court is available$")
+    public void aNewCourtOutdoorIsAvailable() throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/courts/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.available", is(true)))
+                .andExpect(jsonPath("$.indoor", is(false)));
+    }
 
     @And("^A new court has not been created$")
     public void aNewCourtHasNotBeenCreated() throws Throwable {
@@ -51,5 +78,4 @@ public class CreateCourtSteps {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
-
 }
--- src/test/resources/features/CreateCourt.feature
+++ src/test/resources/features/CreateCourt.feature
@@ -3,20 +3,26 @@ Feature: Create court
   As an admin
   I want to create a new court
 
-  Scenario: Create a new court as an admin
+  Scenario: Create a new indoor court as an admin
     Given I login as "admin" with password "password"
-    When I create a new court
+    When I create a new indoor court
     Then The response code is 201
-    And A new court is available
+    And A new indoor court is available
+
+  Scenario: Create a new outdoor court as an admin
+    Given I login as "admin" with password "password"
+    When I create a new outdoor court
+    Then The response code is 201
+    And A new outdoor court is available
 
   Scenario: Create a new court as non admin
     Given I login as "user" with password "password"
-    When I create a new court
+    When I create a new indoor court
     Then The response code is 401
     And A new court has not been created
 
   Scenario: Create a new court without being logged
     Given I'm not logged in
-    When I create a new court
+    When I create a new indoor court
     Then The response code is 401
     And A new court has not been created
