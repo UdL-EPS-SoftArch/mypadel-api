c5e1fe113db35b85e5c772fc95b7bd880a45660f
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-09-26 19:45:30 +0200

    Added Player Entity

--- src/main/java/cat/udl/eps/softarch/mypadel/config/RepositoryRestConfig.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/config/RepositoryRestConfig.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.mypadel.config;
 
 import cat.udl.eps.softarch.mypadel.domain.Admin;
 import cat.udl.eps.softarch.mypadel.domain.MatchResult;
+import cat.udl.eps.softarch.mypadel.domain.Player;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -17,8 +18,9 @@ public class RepositoryRestConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Admin.class);
-        config.exposeIdsFor(MatchResult.class);
-    }
+		config.exposeIdsFor(MatchResult.class);
+		config.exposeIdsFor(Player.class);
+	}
 
     @PostConstruct
     public void init() {
---
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/Player.java
@@ -0,0 +1,7 @@
+package cat.udl.eps.softarch.mypadel.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Player extends User {
+}
