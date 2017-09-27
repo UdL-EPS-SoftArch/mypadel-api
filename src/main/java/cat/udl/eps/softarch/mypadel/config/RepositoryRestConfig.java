c9b813be76a3c8e1a92362b0f3616624edd1f7ce
joanpaucg <joanpau.castells@hotmail.com>
2017-09-27 15:56:21 +0200

    Create Player Feature Done

--- src/main/java/cat/udl/eps/softarch/mypadel/config/RepositoryRestConfig.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/config/RepositoryRestConfig.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.mypadel.config;
 
 import cat.udl.eps.softarch.mypadel.domain.Admin;
+import cat.udl.eps.softarch.mypadel.domain.Player;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -16,6 +17,7 @@ public class RepositoryRestConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Admin.class);
+        config.exposeIdsFor(Player.class);
     }
 
     @PostConstruct
