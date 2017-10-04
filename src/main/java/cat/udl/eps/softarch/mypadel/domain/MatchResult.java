c2b96fcf4d33a474bc2d96721e75708b4ad56f82
poltorres0999 <poltorres0999@gmail.com>
2017-10-04 15:49:57 +0200

    MatchResult class almost finished, i have to ask if we need an atribute for the match we are checking the result, and if we have to implement the verify method.

--- src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResult.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResult.java
@@ -1,26 +1,43 @@
 package cat.udl.eps.softarch.mypadel.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class MatchResult extends UriEntity<Integer> {
 
-    @Id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private boolean isDraw;
+	public List<Player> winningPair;
+
+	public List<Player> loosingPair;
+
+	private boolean isDraw = false;
 
     @Override
     public Integer getId() {
         return this.id;
     }
 
+    public List<Player> getWinningPair () { return this.winningPair; }
+
+    public void setWinningPair (List<Player> winners) { this.winningPair = winners; }
+
+	public List<Player> getLoosingPair () { return this.loosingPair; }
+
+	public void setLoosingPair (List<Player> loosers) { this.winningPair = loosers; }
+
     public boolean isDraw() {
         return isDraw;
     }
+
     public void setDraw(boolean draw) {
         isDraw = draw;
     }
-
+	
 }
