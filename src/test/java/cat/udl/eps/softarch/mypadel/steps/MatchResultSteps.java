a142909649730b573d179ce0a42ccfbcb7201e37
poltorres0999 <poltorres0999@gmail.com>
2017-10-04 18:27:32 +0200

    MatchResult cucumber steps in process.

--- src/test/java/cat/udl/eps/softarch/mypadel/steps/MatchResultSteps.java
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/MatchResultSteps.java
@@ -2,34 +2,32 @@ package cat.udl.eps.softarch.mypadel.steps;
 
 import cat.udl.eps.softarch.mypadel.domain.MatchResult;
 import cat.udl.eps.softarch.mypadel.domain.Player;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import java.util.List;
 
-import static cat.udl.eps.softarch.mypadel.steps.AuthenticationStepDefs.authenticate;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static cat.udl.eps.softarch.mypadel.steps.AuthenticationStepDefs.authenticate;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-/**
- * Created by pta2 on 04/10/2017.
- */
+
 public class MatchResultSteps {
 
 	@Autowired
 	private StepDefs stepDefs;
 
 	@When("^I register a new MatchResult$")
-	public void iCreateANewMatchResult (List<Player> winners, List<Player> loosers, boolean draw) throws Throwable {
+	public void iCreateANewMatchResult (List<Player> winners, List<Player> losers, boolean draw) throws Throwable {
 		MatchResult matchResult = new MatchResult ();
 		matchResult.setWinningPair(winners);
-		matchResult.setLoosingPair(loosers);
+		matchResult.setLoosingPair(losers);
 		matchResult.setDraw(draw);
-		String message = stepDefs.mapper.writeValueAsString(match);
+		String message = stepDefs.mapper.writeValueAsString(matchResult);
 		stepDefs.result = stepDefs.mockMvc.perform(
 			post("/matchResult")
 				.contentType(MediaType.APPLICATION_JSON)
@@ -39,4 +37,17 @@ public class MatchResultSteps {
 			.andDo(print());
 	}
 
+	@And("^A new MatchResult has been created$")
+	public void aMatchResultHasBeenCreated() throws Throwable {
+		int id = 1;
+		stepDefs.result = stepDefs.mockMvc.perform(
+			get("/matchResult/{id}", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.with(authenticate()))
+			.andDo(print())
+			.andExpect(jsonPath("$.id", is(id)))
+			.andExpect(jsonPath("$.winningPair", is(winningPair.toString())))
+			.andExpect(jsonPath("$.losingPair", is(losingPair.toString())))
+			.andExpect(jsonPath("$.isDraw", is(isDraw.toString())));
+	}
 }
