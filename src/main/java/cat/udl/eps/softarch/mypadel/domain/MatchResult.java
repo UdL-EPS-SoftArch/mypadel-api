d4f01a6e030df6972dffc74d9565a852c26942fc
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-10-07 17:36:44 +0200

    JavaDoc on MatchResult TODO talk about a better way to detect who have win/lose/draw a match

--- src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResult.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResult.java
@@ -1,18 +1,21 @@
 package cat.udl.eps.softarch.mypadel.domain;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
+/**
+ * Represents the result of a match
+ */
 @Entity
 @Table(name = "MatchResult")
 public class MatchResult extends UriEntity<Integer> {
 
 	//TODO Add Players and NotNull Checkings
+	//TODO Talk about if having Lists as winning or losing its the best way (also the isDraw property)
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	private int id;
 
 	private ArrayList<Player> winningPair;
 
@@ -22,26 +25,26 @@ public class MatchResult extends UriEntity<Integer> {
 
 	private Match match;
 
-    @Override
-    public Integer getId() {
-        return this.id;
-    }
+	@Override
+	public Integer getId() {
+		return this.id;
+	}
 
-    public ArrayList<Player> getWinningPair () { return this.winningPair; }
+	public ArrayList<Player> getWinningPair () { return this.winningPair; }
 
-    public void setWinningPair (ArrayList<Player> winners) { this.winningPair = winners; }
+	public void setWinningPair (ArrayList<Player> winners) { this.winningPair = winners; }
 
 	public ArrayList<Player> getLosingPair () { return this.losingPair; }
 
 	public void setLosingPair (ArrayList<Player> losers) { this.losingPair = losers; }
 
-    public boolean isDraw() {
-        return isDraw;
-    }
+	public boolean isDraw() {
+		return isDraw;
+	}
 
-    public void setDraw(boolean draw) {
-        isDraw = draw;
-    }
+	public void setDraw(boolean draw) {
+		isDraw = draw;
+	}
 
 	public Match getMatch() {
 		return match;
@@ -50,4 +53,5 @@ public class MatchResult extends UriEntity<Integer> {
 	public void setMatch(Match match) {
 		this.match = match;
 	}
+
 }
