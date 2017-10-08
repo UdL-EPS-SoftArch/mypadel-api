1e134cf6e6028a0aa491bb78e143c6ac9e0f8472
cclauwers <cclauwers@gmail.com>
2017-10-08 23:26:59 +0200

    ReservationStepDefs,fails: Response status expected 201 but was 404

---
+++ src/test/java/cat/udl/eps/softarch/mypadel/steps/ReservationStepDefs.java
@@ -0,0 +1,90 @@
+package cat.udl.eps.softarch.mypadel.steps;
+
+
+import cat.udl.eps.softarch.mypadel.domain.CourtType;
+import cat.udl.eps.softarch.mypadel.domain.Reservation;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static cat.udl.eps.softarch.mypadel.steps.AuthenticationStepDefs.authenticate;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ReservationStepDefs {
+
+	@Autowired
+	private StepDefs stepDefs;
+	private ZonedDateTime startdate;
+	private Duration duration;
+
+	private CourtType courtType;
+
+	@When("^I make a reservation on (\\d+) - (\\d+) - (\\d+) for (\\d+) minutes with CourtType \"([^\"]*)\"$")
+	public void iMakeAReservationOnForMinutesWithCourtType(int day, int month, int year, int duration, String courtType) throws Throwable {
+		this.startdate = ZonedDateTime.of(year, month, day, 0, 0, 0,
+			0, ZoneId.of("+00:00"));
+		this.duration = Duration.ofMinutes(duration);
+
+		Reservation reservation = new Reservation();
+		reservation.setStartDate(startdate);
+		reservation.setDuration(this.duration);
+		reservation.setCourtType(CourtType.valueOf(courtType));
+
+		createReservation(reservation);
+	}
+
+	private void createReservation(Reservation reservation)throws Throwable {
+		String message =stepDefs.mapper.writeValueAsString(reservation);
+		stepDefs.result = stepDefs.mockMvc.perform(
+			post("/reservations")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(message)
+				.accept(MediaType.APPLICATION_JSON)
+				.with(authenticate()))
+			.andDo(print());
+	}
+
+
+	@And("^The reservation is created on (\\d+) - (\\d+) - (\\d+) for (\\d+) minutes with CourtType \"([^\"]*)\"$")
+	public void theReservationIsCreatedOnForMinutesWithCourtType(int day, int month, int year, int duration, String courtType) throws Throwable {
+		int id = 1;
+
+		startdate = ZonedDateTime.of(year, month, day, 0, 0, 0,
+			0, ZoneId.of("+00:00"));
+		this.duration = Duration.ofMinutes(duration);
+
+		stepDefs.result = stepDefs.mockMvc.perform(
+			get("/reservations/{id}",id)
+				.accept(MediaType.APPLICATION_JSON)
+				.with(authenticate()))
+			.andDo(print())
+			.andExpect(jsonPath("$.id",is(id)))
+			.andExpect(jsonPath("$.duration", is(this.duration.toString())))
+			.andExpect(jsonPath("$.startdate", is(parseData(startdate.toString()))))
+			.andExpect(jsonPath("$.courtType", is(CourtType.valueOf(courtType))));
+
+	}
+
+	private String parseData(String data){
+		String[] parts = data.split(":");
+		return parts[0] + ":00:00" + data.substring(data.length()-1);
+	}
+
+	@And("^The reservation can't be created$")
+	public void theReservationCanTBeCreated() throws Throwable {
+		stepDefs.result = stepDefs.mockMvc.perform(
+			get("/reservations/1")
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNotFound());
+	}
+}
