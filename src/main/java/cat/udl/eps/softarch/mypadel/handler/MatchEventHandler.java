c9bf140484d9f3c359aa52317b9b81ebafadaf18
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-10 19:38:52 +0200

    Started the match handler

--- src/main/java/cat/udl/eps/softarch/mypadel/handler/MatchEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/MatchEventHandler.java
@@ -1,4 +1,39 @@
 package cat.udl.eps.softarch.mypadel.handler;
 
+import cat.udl.eps.softarch.mypadel.domain.Admin;
+import cat.udl.eps.softarch.mypadel.domain.Match;
+import cat.udl.eps.softarch.mypadel.domain.Player;
+import cat.udl.eps.softarch.mypadel.repository.AdminRepository;
+import cat.udl.eps.softarch.mypadel.repository.PlayerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.transaction.Transactional;
+
 public class MatchEventHandler {
+	final Logger logger = LoggerFactory.getLogger(Match.class);
+
+	@Autowired
+	AdminRepository adminRepository;
+
+	@Autowired
+	PlayerRepository playerRepository;
+
+	@HandleBeforeCreate
+	@Transactional
+	public void handleMatchPreCreate(Match match){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(adminInputsInvalidPlayer(match, auth)){
+			throw new NullPointerException();
+		}
+		match.setMatchCreator(playerRepository.findOne("player"));
+	}
+
+	private boolean adminInputsInvalidPlayer(Match match, Authentication auth) {
+		return auth.getPrincipal() instanceof Admin && match.getMatchCreator() == null;
+	}
 }
