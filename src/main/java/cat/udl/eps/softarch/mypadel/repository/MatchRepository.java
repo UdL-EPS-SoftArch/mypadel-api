5cc050ae060574e31e621e9bac081add304cb2f3
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-12 13:01:19 +0200

    Changed generic

--- src/main/java/cat/udl/eps/softarch/mypadel/repository/MatchRepository.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/repository/MatchRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface MatchRepository extends PagingAndSortingRepository<Match, Integer> {
+public interface MatchRepository extends PagingAndSortingRepository<Match, Long> {
 }
