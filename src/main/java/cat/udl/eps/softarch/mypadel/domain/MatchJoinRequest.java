bcb232230a2ece57f465f1bbe9b2f0d7de2b4b72
jordi002 <jgm17@alumnes.udl.cat>
2017-10-14 11:21:14 +0200

    CustomMatch Relations defined

---
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchJoinRequest.java
@@ -0,0 +1,21 @@
+package cat.udl.eps.softarch.mypadel.domain;
+
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+
+import javax.persistence.*;
+
+public class MatchJoinRequest extends UriEntity<Long>{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@ManyToOne
+	@JsonIdentityReference(alwaysAsId = true)
+	private CustomMatch customMatch;
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+}
