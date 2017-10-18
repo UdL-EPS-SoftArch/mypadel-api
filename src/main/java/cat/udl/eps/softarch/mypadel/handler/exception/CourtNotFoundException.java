410606ec2b6b1b0e801588803fbd8379e12c8393
Roger Arques <rarquesvall@gmail.com>
2017-10-19 00:21:37 +0200

    Exception and ControllerAdvice for Reservation

---
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/exception/CourtNotFoundException.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.mypadel.handler.exception;
+
+public class CourtNotFoundException extends RuntimeException {
+
+	public CourtNotFoundException(String errorMsg) {
+		super(errorMsg);
+	}
+
+}
