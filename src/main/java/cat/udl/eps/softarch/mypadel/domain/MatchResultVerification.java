f4c5342dd74b18a042116e37e41a2c7a7f24f642
pta2 <poltorres0999@gmail.com>
2017-10-18 16:04:09 +0200

    Relations between match, matchResult, and MatchResultVerification 1.0.2

--- src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResultVerification.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/MatchResultVerification.java
@@ -2,11 +2,15 @@ package cat.udl.eps.softarch.mypadel.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  *
  */
+@Entity
+@Table(name = "MatchResultVerification")
 public class MatchResultVerification {
 
 	private  boolean agrees;
