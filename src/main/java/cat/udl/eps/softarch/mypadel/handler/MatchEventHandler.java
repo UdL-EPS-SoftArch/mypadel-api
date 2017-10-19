ca5a5dab431468002b0ceae7dcac772f9478ef24
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-19 11:20:04 +0200

    Deleted unused settings

--- src/main/java/cat/udl/eps/softarch/mypadel/handler/MatchEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/MatchEventHandler.java
@@ -3,30 +3,19 @@ package cat.udl.eps.softarch.mypadel.handler;
 import cat.udl.eps.softarch.mypadel.domain.Admin;
 import cat.udl.eps.softarch.mypadel.domain.Match;
 import cat.udl.eps.softarch.mypadel.domain.Player;
-import cat.udl.eps.softarch.mypadel.repository.AdminRepository;
-import cat.udl.eps.softarch.mypadel.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.time.Duration;
-import java.time.ZonedDateTime;
 
 @Component
 @RepositoryEventHandler
 public class MatchEventHandler {
 	final Logger logger = LoggerFactory.getLogger(Match.class);
-
-	@Autowired
-	AdminRepository adminRepository;
-
-	@Autowired
-	PlayerRepository playerRepository;
 
 	@HandleBeforeCreate
 	@Transactional
