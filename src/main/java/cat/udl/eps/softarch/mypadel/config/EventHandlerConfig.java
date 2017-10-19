7ce488e627ed45298ecec7cd11ed36bf432879ba
rhc1 <raul.hidalgo.sopa@gmail.com>
2017-10-19 16:03:25 +0200

    MatchResultVerificationEventListener

--- src/main/java/cat/udl/eps/softarch/mypadel/config/EventHandlerConfig.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/config/EventHandlerConfig.java
@@ -2,20 +2,31 @@ package cat.udl.eps.softarch.mypadel.config;
 
 import cat.udl.eps.softarch.mypadel.eventlistener.AdminEventListener;
 import cat.udl.eps.softarch.mypadel.eventlistener.MatchResultEventListener;
+import cat.udl.eps.softarch.mypadel.eventlistener.MatchResultVerificationEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.validation.constraints.NotNull;
 
 @Configuration
 public class EventHandlerConfig {
 
 	@Bean
+	@NotNull
 	public AdminEventListener adminEventListener() {
 		return new AdminEventListener();
 	}
 
 	@Bean
+	@NotNull
 	public MatchResultEventListener matchResultEventListener() {
 		return new MatchResultEventListener();
+	}
+
+	@Bean
+	@NotNull
+	public MatchResultVerificationEventListener matchResultVerificationEventListener() {
+		return new MatchResultVerificationEventListener();
 	}
 
 }
--- src/main/java/cat/udl/eps/softarch/mypadel/eventlistener/MatchResultVerificationEventListener.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/eventlistener/MatchResultVerificationEventListener.java
@@ -1,18 +1,18 @@
 package cat.udl.eps.softarch.mypadel.eventlistener;
 
-import cat.udl.eps.softarch.mypadel.domain.MatchResult;
+import cat.udl.eps.softarch.mypadel.domain.MatchResultVerification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
 
 import javax.validation.constraints.NotNull;
 
-public class MatchResultEventListener extends AbstractRepositoryEventListener<MatchResult> {
+public class MatchResultVerificationEventListener extends AbstractRepositoryEventListener<MatchResultVerification> {
 
-	private final Logger logger = LoggerFactory.getLogger(MatchResult.class);
+	private final Logger logger = LoggerFactory.getLogger(MatchResultVerification.class);
 
 	@Override
-	protected void onBeforeCreate(@NotNull MatchResult entity) {
+	protected void onBeforeCreate(@NotNull MatchResultVerification entity) {
 		super.onBeforeCreate(entity);
 		logger.info("Before creating: {}", entity.toString());
 	}
