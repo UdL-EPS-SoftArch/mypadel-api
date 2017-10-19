55d1f46589066006e78ff3be80d0dabb42a36014
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-19 16:13:39 +0200

    Added a new exception

---
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/exception/createMatchException.java
@@ -0,0 +1,8 @@
+package cat.udl.eps.softarch.mypadel.handler.exception;
+
+public class createMatchException extends Throwable {
+
+	public createMatchException(String message) {
+		super(message);
+	}
+}
