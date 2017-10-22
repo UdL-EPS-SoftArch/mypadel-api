f1391148b8fe61239e05d3a44810484c7f337c6c
ratchetmdt <mdt3@alumnes.udl.cat>
2017-10-22 16:12:09 +0200

    Checked reservation is not overlapping with current reservations

--- src/main/java/cat/udl/eps/softarch/mypadel/handler/CourtFilter.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/CourtFilter.java
@@ -2,6 +2,8 @@ package cat.udl.eps.softarch.mypadel.handler;
 
 import cat.udl.eps.softarch.mypadel.domain.Court;
 import cat.udl.eps.softarch.mypadel.domain.Reservation;
+
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,9 +30,22 @@ public class CourtFilter {
 
 	private boolean isOverlappingReservation(Court court, Reservation pendingReservation) {
 		List<Reservation> assignedReservations = court.getReservations();
+		ZonedDateTime pendingReservationStartDate = pendingReservation.getStartDate();
+		ZonedDateTime pendingReservationEndDate = pendingReservationStartDate.plus(pendingReservation.getDuration());
+		boolean overlapping = false;
 		for (Reservation reservation : assignedReservations) {
+			ZonedDateTime reservationStartDate = reservation.getStartDate();
+			ZonedDateTime reservationEndDate = reservationStartDate.plus(reservation.getDuration());
+			if(reservationEndDate.isBefore(pendingReservationStartDate)){
+				overlapping = false;
+			} else if (reservationStartDate.isAfter(pendingReservationEndDate)) {
+				overlapping = false;
+			} else {
+				overlapping = true;
+			}
 
 		}
+		return overlapping;
 	}
 
 }
