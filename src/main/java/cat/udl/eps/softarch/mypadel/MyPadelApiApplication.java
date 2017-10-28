17f7de199934d45655ad1d24e87179849eff6404
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-28 19:20:30 +0200

    added enable scheduling annotation

--- src/main/java/cat/udl/eps/softarch/mypadel/MyPadelApiApplication.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/MyPadelApiApplication.java
@@ -2,8 +2,10 @@ package cat.udl.eps.softarch.mypadel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyPadelApiApplication {
 
 	public static void main(String[] args) {
