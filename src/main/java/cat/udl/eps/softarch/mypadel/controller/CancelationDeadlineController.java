07d629ba1074eb7e151c62164bd2341051325c5a
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-29 16:52:55 +0100

    getting matches with less than 4 players

--- src/main/java/cat/udl/eps/softarch/mypadel/controller/CancelationDeadlineController.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/controller/CancelationDeadlineController.java
@@ -1,6 +1,8 @@
 package cat.udl.eps.softarch.mypadel.controller;
 
+import cat.udl.eps.softarch.mypadel.domain.JoinMatch;
 import cat.udl.eps.softarch.mypadel.domain.Match;
+import cat.udl.eps.softarch.mypadel.repository.JoinMatchRepository;
 import cat.udl.eps.softarch.mypadel.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,18 +14,34 @@ import java.util.List;
 @Component
 public class CancelationDeadlineController {
 
+	private static int REVIEW_TIME = 30;
+
 	@Autowired
 	private MatchRepository matchRepository;
+
+	@Autowired
+	private JoinMatchRepository joinMatchRepository;
 
 	//This method will be called every 30 minutes after the last completion of this method.
 	@Scheduled(fixedDelay = 1800000)
 	public void searchReachedDeadlines() {
-		List<Match> matchesToReview = matchRepository.findByCancelationDeadlineBetween(
-			ZonedDateTime.now().minusMinutes(30), ZonedDateTime.now());
+		List<Match> matchesToReview = getPossibleCancelledMatches();
+		reviewMatches(matchesToReview);
+	}
 
-		//get the players on the match
-		//not 4 joined
-		//cancel match
-		//send mail
+	private List<Match> getPossibleCancelledMatches() {
+		return matchRepository.findByCancelationDeadlineBetween(
+			ZonedDateTime.now().minusMinutes(REVIEW_TIME), ZonedDateTime.now());
+	}
+
+	private void reviewMatches(List<Match> matchesToReview) {
+		for (Match reviewedMatch : matchesToReview) {
+			List<JoinMatch> joinMatches = joinMatchRepository.findByMatch(reviewedMatch);
+			if (joinMatches.size() != 4){
+//				cancelMatch();
+//				sendMail();
+			}
+
+		}
 	}
 }
