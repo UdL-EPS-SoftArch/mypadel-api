12e597ed410d70289fcb4974cbee4dd25d3de76f
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-30 13:25:11 +0100

    implementing the cancel deadline controller

--- src/main/java/cat/udl/eps/softarch/mypadel/controller/CancelationDeadlineController.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/controller/CancelationDeadlineController.java
@@ -1,10 +1,14 @@
 package cat.udl.eps.softarch.mypadel.controller;
 
+import cat.udl.eps.softarch.mypadel.config.MailConfig;
 import cat.udl.eps.softarch.mypadel.domain.JoinMatch;
 import cat.udl.eps.softarch.mypadel.domain.Match;
+import cat.udl.eps.softarch.mypadel.domain.Player;
 import cat.udl.eps.softarch.mypadel.repository.JoinMatchRepository;
 import cat.udl.eps.softarch.mypadel.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -37,11 +41,33 @@ public class CancelationDeadlineController {
 	private void reviewMatches(List<Match> matchesToReview) {
 		for (Match reviewedMatch : matchesToReview) {
 			List<JoinMatch> joinMatches = joinMatchRepository.findByMatch(reviewedMatch);
-			if (joinMatches.size() != 4){
-//				cancelMatch();
-//				sendMail();
+			if (joinMatches.size() < 4){
+				cancelMatch(reviewedMatch);
+				sendMailToPlayers(joinMatches);
 			}
-
 		}
+	}
+
+	private void cancelMatch(Match reviewedMatch) {
+		reviewedMatch.setCancelled(true);
+		//more
+	}
+
+	private void sendMailToPlayers(List<JoinMatch> joinMatches) {
+		for(JoinMatch joinMatch : joinMatches){
+			Player player = joinMatch.getPlayer();
+			Match match = joinMatch.getMatch();
+			String matchDate = match.getStartDate().toString();
+			JavaMailSender mailSender = MailConfig.getJavaMailSender();
+			mailSender.send(getMessage(player, matchDate));
+		}
+	}
+
+	private SimpleMailMessage getMessage(Player player, String matchDate) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(player.getEmail());
+		message.setSubject("Padle match cancelled");
+		message.setText("The match on "+ matchDate +" has been cancelled. Less than four players joined.");
+		return message;
 	}
 }
