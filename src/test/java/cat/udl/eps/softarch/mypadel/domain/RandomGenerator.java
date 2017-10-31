77312920dc2ce7b567db0eedc4214e01cbe4023e
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-10-31 23:49:04 +0100

    better generator of entities

--- src/test/java/cat/udl/eps/softarch/mypadel/domain/RandomGenerator.java
+++ src/test/java/cat/udl/eps/softarch/mypadel/domain/RandomGenerator.java
@@ -1,19 +1,26 @@
 package cat.udl.eps.softarch.mypadel.domain;
 
 import javax.validation.constraints.NotNull;
+import java.util.Random;
 
 public class RandomGenerator {
 
+	@NotNull
+	private final Random random=new Random();
+
+	@NotNull
 	public MatchResult generateMatchResult() {
-		MatchResult matchResult = new MatchResult();
+		final MatchResult matchResult = new MatchResult();
+		matchResult.setDraw(random.nextBoolean());
+		matchResult.setVerified(random.nextBoolean());
 		return matchResult;
 	}
 
 	@NotNull
 	public MatchResultVerification generateMatchResultVerification() {
-		MatchResultVerification matchResultVerification = new MatchResultVerification();
+		final MatchResultVerification matchResultVerification = new MatchResultVerification();
 		matchResultVerification.setMatchToAgree(generateMatchResult());
-		matchResultVerification.setAgrees(true);
+		matchResultVerification.setAgrees(random.nextBoolean());
 		matchResultVerification.setPlayer(new Player());
 		return matchResultVerification;
 	}
