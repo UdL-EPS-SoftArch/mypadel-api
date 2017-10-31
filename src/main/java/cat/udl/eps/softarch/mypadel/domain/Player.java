4a6a2646571e9a43e594e6d927c32a380117aec2
bergacat1 <albertbergagatius@gmail.com>
2017-10-31 19:45:01 +0100

    Removing new lines

--- src/main/java/cat/udl/eps/softarch/mypadel/domain/Player.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/domain/Player.java
@@ -38,9 +38,6 @@ public class Player extends User {
 	@JsonIdentityReference(alwaysAsId = true)
 	private List<MatchJoinRequest> matchJoinRequests = new ArrayList<>();
 
-
-
-
     @OneToMany (mappedBy = "invites")
 	@JsonIdentityReference(alwaysAsId = true)
 	private List<MatchInvitation> invitations = new ArrayList<>();
