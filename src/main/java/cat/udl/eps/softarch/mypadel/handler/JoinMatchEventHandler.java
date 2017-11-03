c91f48c34c3df1bb7050d8be269019b5a3779144
Albert Mari <amr1990lol@gmail.com>
2017-11-03 21:16:20 +0100

    Removed unused imports and commented code

--- src/main/java/cat/udl/eps/softarch/mypadel/handler/JoinMatchEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/handler/JoinMatchEventHandler.java
@@ -1,23 +1,16 @@
 package cat.udl.eps.softarch.mypadel.handler;
 
 import cat.udl.eps.softarch.mypadel.domain.JoinMatch;
-import cat.udl.eps.softarch.mypadel.domain.MatchInvitation;
-import cat.udl.eps.softarch.mypadel.domain.PrivateMatch;
 import cat.udl.eps.softarch.mypadel.handler.exception.MissingInvitationException;
-import cat.udl.eps.softarch.mypadel.repository.MatchInvitationRepository;
-import cat.udl.eps.softarch.mypadel.repository.MatchRepository;
-import cat.udl.eps.softarch.mypadel.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.List;
 
 @Component
 @RepositoryEventHandler
@@ -39,19 +32,5 @@ public class JoinMatchEventHandler {
 		if(!invitationChecker.isInvited(joinMatch)){
 			throw new MissingInvitationException("You have not been invited to this match");
 		}
-		/*if(joinMatch.getMatch() instanceof PrivateMatch) {
-			List<MatchInvitation> matchInvitationList = joinMatch.getMatch().getInvitations();
-			if(matchInvitationList.size() == 0){
-				throw new MissingInvitationException("There is no invitations for this match");
-			}
-			for (MatchInvitation matchInvitation : matchInvitationList) {
-				if(matchInvitation.getInvites().getEmail().equals(joinMatch.getPlayer().getEmail())){
-					invite = true;
-				}
-			}
-			if(!invite){
-				throw new MissingInvitationException("You have not been invited to this match");
-			}
-		}*/
 	}
 }
