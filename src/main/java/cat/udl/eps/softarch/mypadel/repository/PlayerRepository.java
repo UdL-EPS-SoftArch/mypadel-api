63d1de82a7375ff3737ce3f09ab9546f14934f11
joanpaucg <joanpau.castells@hotmail.com>
2017-12-02 21:23:06 +0100

    Player findByUserName

--- src/main/java/cat/udl/eps/softarch/mypadel/repository/PlayerRepository.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/repository/PlayerRepository.java
@@ -2,9 +2,12 @@ package cat.udl.eps.softarch.mypadel.repository;
 
 import cat.udl.eps.softarch.mypadel.domain.Player;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface PlayerRepository  extends PagingAndSortingRepository<Player, String> {
-
+	List<Player> findByUsernameContaining(@Param("text") String text);
 }
