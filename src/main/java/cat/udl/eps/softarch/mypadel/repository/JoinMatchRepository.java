4033b4d6d54312cc860005872df72cd37dfaafd7
Albert Mari <amr1990lol@gmail.com>
2017-12-20 17:54:01 +0100

    Added annotation @Param to findByPlayer

--- src/main/java/cat/udl/eps/softarch/mypadel/repository/JoinMatchRepository.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/repository/JoinMatchRepository.java
@@ -4,6 +4,7 @@ import cat.udl.eps.softarch.mypadel.domain.JoinMatch;
 import cat.udl.eps.softarch.mypadel.domain.Match;
 import cat.udl.eps.softarch.mypadel.domain.Player;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 @RepositoryRestResource
 public interface JoinMatchRepository extends PagingAndSortingRepository<JoinMatch, Long> {
 	List<JoinMatch> findByMatch(Match match);
-	List<JoinMatch> findByPlayer(Player player);
+	List<JoinMatch> findByPlayer(@Param("text") Player player);
 }
