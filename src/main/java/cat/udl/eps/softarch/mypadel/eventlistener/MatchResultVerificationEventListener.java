126ea466736ffa42dc0cc394bde8c6ae561a04c1
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-01-09 17:25:01 +0100

    Added Autowired Repositories

--- src/main/java/cat/udl/eps/softarch/mypadel/eventlistener/MatchResultVerificationEventListener.java
+++ src/main/java/cat/udl/eps/softarch/mypadel/eventlistener/MatchResultVerificationEventListener.java
@@ -1,8 +1,11 @@
 package cat.udl.eps.softarch.mypadel.eventlistener;
 
 import cat.udl.eps.softarch.mypadel.domain.MatchResultVerification;
+import cat.udl.eps.softarch.mypadel.repository.MatchResultRepository;
+import cat.udl.eps.softarch.mypadel.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
 
 import javax.validation.constraints.NotNull;
@@ -11,10 +14,16 @@ public class MatchResultVerificationEventListener extends AbstractRepositoryEven
 
 	private final Logger logger = LoggerFactory.getLogger(MatchResultVerification.class);
 
+	@Autowired
+	private MatchResultRepository matchResultRepository;
+
+	@Autowired
+	private PlayerRepository playerRepositoryRepository;
+
 	@Override
 	protected void onBeforeCreate(@NotNull MatchResultVerification entity) {
 		super.onBeforeCreate(entity);
-		logger.info("Before creating: {}", entity.toString());
+		this.logger.info("Before creating: {}", entity.toString());
 	}
 
 }
